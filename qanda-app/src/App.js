4900ba42ddb9b719942a6e21a10f99220dcba310
swapnil chavan <chavanswapnil822@gmail.com>
2024-03-18 15:06:09 +0530

    updated data

--- qanda-app/src/App.js
+++ qanda-app/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Scheduler from './components/scheduler.js';
@@ -9,8 +9,7 @@ import { VideoPlayer } from './components/videoPlayer.js';
 import UploadForm from './components/videoUploadForm.js';
 import { UserResponse } from './components/userResponse.js';
 import { Demo } from './components/demo.js';
-
-
+import RegistrationForm from './components/registartionForm.js';
 
 function App() {
   
@@ -26,7 +25,7 @@ function App() {
           <Route path="theateroperator" element={<Theateroperator />} />
           <Route path="video-player" element={<VideoPlayer />} />
           <Route path="userResponse" element={<UserResponse />} />
-
+          <Route path="register" element={<RegistrationForm />} />
       </Routes>
     </BrowserRouter>
 
